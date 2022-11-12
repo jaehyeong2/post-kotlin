f84aff9bec22db2497fecbce8ec8ae99cb2e962d
jaehyeong2 <wogud1514@gmail.com>
2022-11-12 17:04:49 +0900

    start

---
+++ src/main/kotlin/jjfactory/post/PostApplication.kt
@@ -0,0 +1,11 @@
+package jjfactory.post
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PostApplication
+
+fun main(args: Array<String>) {
+    runApplication<PostApplication>(*args)
+}
